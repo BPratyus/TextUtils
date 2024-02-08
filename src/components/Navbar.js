1af0e29de5cc672cbe2ac75bc2071088f5389df5
Pratyus <basulipratyus@gmail.com>
2024-02-08 19:59:50 +0530

    fixed minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar(props) { //props are read-only and can be any objects
   return (
     <div >
-    <nav className="navbar navbar-expand-lg navbar bg-body" data-bs-theme={props.mode} >
-    {/* data-bs-theme={props.mode} */}
+    <nav className="navbar navbar-expand-lg navbar bg-body-tertiary" data-bs-theme={props.mode} >
     <div className="container-fluid"  >
       <Link className="navbar-brand" to="/">{props.title}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
